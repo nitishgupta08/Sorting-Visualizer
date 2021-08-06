5cfe2ac9721022e32fbc0d3f7915fefe2083a152
nitishgupta <nitishkumargupta.1729@gmail.com>
2021-08-06 11:36:34 +0530

    Update quicksort.js
    Made quicksort work

--- scripts/quicksort.js
+++ scripts/quicksort.js
@@ -1,48 +1,54 @@
-async function partition() { }
+async function partition(arr, l, r) {
+    console.log("In partition function");
+    let pivot = r;
+    let i = l - 1;
+    arr[pivot].style.background = '#f9259d';
+
+    for (let j = l; j <= r - 1; j++) {
+        arr[j].style.background = '#9df925';
+        await animate(waitTime);
+        if (parseInt(arr[j].style.height) < parseInt(arr[pivot].style.height)) {
+            i++;
+            arr[i].style.background = '#f92533';
+            arr[j].style.background = '#f92533';
+            await animate(waitTime);
+            swap(arr[i], arr[j]);
+            arr[i].style.background = '#259df9';
+            arr[j].style.background = '#259df9';
+            await animate(waitTime);
+        }
+    }
+    swap(arr[++i], arr[r]);
 
 
+    await animate(waitTime);
+
+    for (let k = 0; k <= pivot; k++)
+        arr[k].style.background = '#25f9eb';
+
+    for (let k = pivot + 1; k < arr.length; k++)
+        arr[k].style.background = '#25f9eb';
 
 
+    return i;
+}
 
-// //left-most element is pivot
-// int partition(int * arr, int p, int r) {
-//     int pivot = arr[p];
-//     int i = p;
-//     int j = r;
+async function quickSort(arr, l, r) {
+    if (l < r) {
+        let pivot_index = await partition(arr, l, r);
+        await quickSort(arr, l, pivot_index - 1);
+        await quickSort(arr, pivot_index + 1, r);
+    }
+}
 
-//     while (i < j) {
-//         while (arr[i] <= pivot && i <= r)
-//             i = i + 1;
-//         while (arr[j] > pivot && j >= p)
-//             j = j - 1;
-//         if (i < j)
-//             swap(arr[i], arr[j]);
-//     }
-//     swap(arr[p], arr[j]);
-//     return j;
-// }
-
-// void QuickSort(int * arr, int p, int r) {
-//     if (p < r) {
-//         int q = partition(arr, p, r);
-//         QuickSort(arr, p, q - 1);
-//         QuickSort(arr, q + 1, r);
-//     }
-// }
-
-// int main() {
-//     int n;
-//     cout << "Enter number of elements: ";
-//     cin >> n;
-//     int A[n];
-//     cout << "Enter array elements: ";
-//     for (int i = 0; i < n; i++)
-//     cin >> A[i];
-
-//     QuickSort(A, 0, n - 1);
-
-//     cout << "Sorted Array(Quick Sort): ";
-//     for (int i = 0; i < n; i++)
-//     cout << A[i] << " ";
-//     cout << "\n";
-// }
+async function quick_sort() {
+    console.log("Quick sort running");
+    disableInputs();
+    let arr = document.querySelectorAll('.bar');
+    await quickSort(arr, 0, parseInt(arr.length) - 1);
+    for (let k = arr.length - 1; k >= 0; k--) {
+        arr[k].style.background = '#f98125';
+        await animate(100 / arr.length);
+    }
+    enableInputs();
+}
